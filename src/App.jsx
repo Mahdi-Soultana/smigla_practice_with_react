f513ed952b1da30cb6450d911046d8827bb050ed
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-11-25 20:56:41 -0600

    Lorem ipsum init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/From/Form";
+
+function App() {
+  return <Form />;
+}
+
+export default App;
